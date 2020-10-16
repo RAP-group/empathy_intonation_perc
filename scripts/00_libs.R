94250d666a2343ca3ba68b265997fb4b8e370c79
jvcasillas <joseph.casillas@rutgers.edu>
2020-10-15 23:54:07 -0400

    create libs script

---
+++ scripts/00_libs.R
@@ -0,0 +1,11 @@
+# Load libraries --------------------------------------------------------------
+
+library("here")
+library("fs")
+library("dplyr")
+library("tidyr")
+library("readr")
+library("readxl")
+library("writexl")
+
+# -----------------------------------------------------------------------------
