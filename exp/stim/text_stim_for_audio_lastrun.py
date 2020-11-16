69bf61f2aace4c72801c24e3c2086dddd4f459df
jvcasillas <joseph.casillas@rutgers.edu>
2020-11-16 12:02:07 -0500

    update stim list to include only 8 syllable utterances

--- exp/stim/text_stim_for_audio_lastrun.py
+++ exp/stim/text_stim_for_audio_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.5),
-    on Sat Nov 14 07:11:25 2020
+    on Mon Nov 16 11:58:43 2020
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
