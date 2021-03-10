8f66f5f4740c89c7a1a382339a9c305546586ba0
jvcasillas <joseph.casillas@rutgers.edu>
2021-03-10 00:04:45 -0500

    small data, add itme check

--- scripts/r/11_small_data.R
+++ scripts/r/11_small_data.R
@@ -10,3 +10,21 @@ sr %>%
   pivot_longer(cols = c("avg_val", "med_val"), names_to = "measure", 
     values_to = "val") %>% 
   printy::super_split(speaker_variety, metric)
+
+
+
+
+time <- read_csv(here("data", "raw", "prolific_export_602ab2f3ccc37d12d58ba79b.csv")) %>% 
+  select(status, time_taken) %>% 
+  filter(status == "APPROVED") %>% 
+  mutate(min = time_taken / 60) 
+
+t_mean   <- time$min %>% mean
+t_median <- time$min %>% median
+
+
+time %>% 
+  ggplot() + 
+    aes(x = min) + 
+    geom_histogram(binwidth = 1, color = "black", fill = "grey90") + 
+    geom_vline(xintercept = t_median, lty = 3)
