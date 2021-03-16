4756af259920d970104eb49737adec80a45076cb
jvcasillas <joseph.casillas@rutgers.edu>
2021-03-16 15:14:50 -0400

    udpate mods

--- scripts/r/08_bda.R
+++ scripts/r/08_bda.R
@@ -13,6 +13,10 @@ source(here::here("scripts", "r", "07_load_data.R"))
 # -----------------------------------------------------------------------------
 
 
+
+
+# Sample from prior distribution ----------------------------------------------
+
 # Regularizing, weakly informative priors
 nat_priors <- c(
   prior(normal(0, 2), class = "Intercept"), 
@@ -21,7 +25,8 @@ nat_priors <- c(
 
 native_ppred_response_00 <- brm(
   is_correct ~ 1 + 
-    (1 | participant ) + (1 | sentence) + (1 | speaker_variety), 
+    (1 | participant ) + (1 | speaker_variety/sentence_type/sentence) + 
+    (1 | sentence_type), 
   data = natives, 
   prior = nat_priors, 
   sample_prior = "only",
@@ -33,7 +38,8 @@ native_ppred_response_00 <- brm(
 
 native_ppred_rt_00 <- brm(
   rt_adj ~ 1 + 
-    (1 | participant ) + (1 | sentence) + (1 | speaker_variety), 
+    (1 | participant ) + (1 | speaker_variety/sentence_type/sentence) + 
+    (1 | sentence_type), 
   data = natives %>% filter(rt_adj > 0), 
   prior = nat_priors, 
   sample_prior = "only",
@@ -46,14 +52,17 @@ native_ppred_rt_00 <- brm(
 ppred_response_samples <- posterior_samples(native_ppred_response_00) %>%
   rename(intercept = b_Intercept, 
          participant_sd = sd_participant__Intercept, 
-         sentence_sd = sd_sentence__Intercept, 
-         speaker_variety_sd = sd_speaker_variety__Intercept)
+         sentence_type_sd = sd_sentence_type__Intercept, 
+         speaker_variety_sd = sd_speaker_variety__Intercept, 
+         speaker_variety_sentence_type_sd = `sd_speaker_variety:sentence_type__Intercept`)
+
 
 ppred_rt_samples <- posterior_samples(native_ppred_rt_00) %>%
   rename(intercept = b_Intercept, 
          participant_sd = sd_participant__Intercept, 
-         sentence_sd = sd_sentence__Intercept, 
-         speaker_variety_sd = sd_speaker_variety__Intercept)
+         sentence_type_sd = sd_sentence_type__Intercept, 
+         speaker_variety_sd = sd_speaker_variety__Intercept, 
+         speaker_variety_sentence_type_sd = `sd_speaker_variety:sentence_type__Intercept`)
 
 ppred_response_samples %>%
   pivot_longer(intercept:speaker_variety_sd, names_to = "term") %>% 
@@ -69,40 +78,81 @@ ppred_rt_samples %>%
   geom_density(colour = NA, fill = "#325756", alpha = 0.9) +
   facet_wrap(~ term, scales = "free")
 
+# -----------------------------------------------------------------------------
 
-# Fit model
-native_response_00 <- brm(
+
+
+
+
+# Fit model -------------------------------------------------------------------
+
+native_response_01 <- brm(
   is_correct ~ 1 + 
-    (1 | participant ) + (1 | sentence) + (1 | speaker_variety), 
+    (1 | participant ) + (1 | speaker_variety/sentence_type/sentence) + 
+    (1 | sentence_type), 
   data = natives, 
   prior = nat_priors, 
   warmup = 1000, iter = 2000, chains = 6, 
   family = "bernoulli", 
   cores = parallel::detectCores(), 
   control = list(adapt_delta = 0.99), 
-  file = here("models", "native_response_00")
+  file = here("models", "native_response_01")
 )
 
-pp_check(native_response_00, nsamples = 200)
-pairs(native_response_00)
+formula <- bf(
+  rt_adj ~ 0 + Intercept + 
+    (1 | participant) + (1 | speaker_variety/sentence_type/sentence) + 
+    (1 | sentence_type), 
+  ndt ~ 0 + Intercept + (1 | participant)
+  )
 
-post_samples <- posterior_samples(native_response_00) %>%
+rt_slog_priors <- c(
+  set_prior('normal(-0.69, 0.5)', class = 'Intercept'), # around  0.500 secs
+  set_prior("normal(0, 0.5)",     class = "Intercept", dpar = "ndt"),
+  set_prior('normal(0.4, 0.3)',   class = 'sigma'),        # SD of individual rts in log-units
+  set_prior('normal(0.3, 0.1)',   class = 'sd')            # variability between participants
+)
+
+native_rt_01 <- brm(
+  formula = formula, 
+  data = natives %>% filter(rt_adj > 0 & rt_adj <= 5), 
+  #prior = rt_slog_priors, 
+  warmup = 1000, iter = 2000, chains = 6, 
+  family = shifted_lognormal(), 
+  cores = 4, 
+  file = here("models", "native_rt_01")
+)
+
+pp_check(native_response_01, nsamples = 200)
+pp_check(native_rt_01, nsamples = 200)
+
+# -----------------------------------------------------------------------------
+
+
+
+
+
+
+
+post_samples <- posterior_samples(native_response_01) %>% glimpse
   rename(intercept = b_Intercept, 
          participant_sd = sd_participant__Intercept, 
-         sentence_sd = sd_sentence__Intercept, 
-         speaker_variety_sd = sd_speaker_variety__Intercept)
+         sentence_type_sd = sd_sentence_type__Intercept, 
+         speaker_variety_sd = sd_speaker_variety__Intercept, 
+         speaker_variety_sentence_type_sd = `sd_speaker_variety:sentence_type__Intercept`)
 
 # Grouping variable posteriors
-posterior_samples(native_response_00) %>% 
+posterior_samples(native_response_01) %>% 
   select(starts_with("sd")) %>% 
   gather(key, sd) %>% 
   mutate(key = str_remove(key, "sd_") %>% 
            str_remove(., "__Intercept")) %>% 
   ggplot(aes(x = sd, fill = key)) +
-  geom_density(color = "transparent", alpha = 0.9) +
-  scale_fill_viridis_d(name = "Group-level variance", option = "D", end = 0.6, 
-    labels = c("Participant", "Sentence", "Speaker variety")) + 
-  coord_cartesian(xlim = c(0, 3)) + 
+  geom_density(color = "white", alpha = 0.7) +
+  scale_fill_viridis_d(name = "Group-level variance", option = "D", end = 0.9, 
+    labels = c("Participant", "Sentence type", "Speaker variety", 
+    "Speaker variety:sentence type", "Speaker variety:sentence type:sentence")) + 
+  coord_cartesian(xlim = c(0, 4)) + 
   labs(y = "Density", x = expression(beta)) + 
   minimal_adj() + 
   theme(legend.position = c(0.85, 0.75), legend.title = element_text(size = 9), 
@@ -115,12 +165,12 @@ posterior_samples(native_response_00) %>%
 
 # Get draws for each study
 participant_draws <- 
-  spread_draws(native_response_00, r_participant[Participant,], b_Intercept) %>% 
+  spread_draws(native_response_01, r_participant[Participant,], b_Intercept) %>% 
   mutate(b_Intercept = r_participant + b_Intercept)
 
 # Get draws for pooled effect
 participant_pooled_effect_draws <- 
-  spread_draws(native_response_00, b_Intercept) %>% 
+  spread_draws(native_response_01, b_Intercept) %>% 
   mutate(Participant = "Pooled Effect")
 
 # Combine it and clean up
@@ -157,69 +207,10 @@ p_post_participant <- participant_forest_data %>%
     alpha = 0.75, height = Inf, fill = "#31688EFF") +
   stat_pointinterval(point_fill = "white", shape = 21, alpha = 0.85, 
     .width = c(0.8, 0.95), point_size = 1.5) +
-  coord_cartesian(xlim = c(0, 5)) + 
+  coord_cartesian(xlim = c(0, 7)) + 
   labs(x = expression(beta), y = NULL) +
   theme_test(base_size = 12) + 
   theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
-
-
-
-
-
-
-
-
-# By sentence
-
-# Get draws for each study
-sentence_draws <- 
-  spread_draws(native_response_00, r_sentence[Sentence,], b_Intercept) %>% 
-  mutate(b_Intercept = r_sentence + b_Intercept)
-
-# Get draws for pooled effect
-sentence_pooled_effect_draws <- 
-  spread_draws(native_response_00, b_Intercept) %>% 
-  mutate(Sentence = "Pooled Effect")
-
-# Combine it and clean up
-sentence_forest_data <- 
-  bind_rows(sentence_draws, sentence_pooled_effect_draws) %>% 
-  ungroup() %>%
-  mutate(Sentence = reorder(Sentence, b_Intercept), 
-         Sentence = relevel(Sentence, "Pooled Effect", after = Inf))
-
-# Calculate mean qi intervals for right margin text
-sentence_forest_data_summary <- 
-  group_by(sentence_forest_data, Sentence) %>% 
-  mean_qi(b_Intercept, .width = 0.95) 
-
-# Calculate mean qi intervals for pooled effect
-sentence_pooled_summary <- 
-  group_by(sentence_forest_data, Sentence) %>% 
-  mean_qi(b_Intercept, .width = c(0.5, 0.8, 0.95)) %>% 
-  filter(Sentence == "Pooled Effect")
-
-# Plot it all
-p_post_sentence <- sentence_forest_data %>% 
-  ggplot() + 
-  aes(x = b_Intercept, y = Sentence) + 
-  geom_text(data = 
-    mutate_if(sentence_forest_data_summary, is.numeric, round, 2) %>% 
-    mutate_at(c("b_Intercept", ".lower", ".upper"), as.character) %>% 
-    mutate_at(c("b_Intercept", ".lower", ".upper"), unicode_minus) %>% 
-    mutate_at(c("b_Intercept", ".lower", ".upper"), strip_blank),
-            aes(label = glue("{b_Intercept} [{.lower}, {.upper}]"), x = Inf), 
-            hjust = "inward", family = "Times") + 
-  geom_tile(data = sentence_pooled_summary, aes(width = .lower - .upper),
-    alpha = 0.2, height = Inf, fill = "#31688EFF") +
-  stat_pointinterval(point_fill = "white", shape = 21, .width = c(0.8, 0.95)) +
-  coord_cartesian(xlim = c(0, 6)) + 
-  labs(x = expression(italic("beta")), y = NULL) +
-  minimal_adj(base_size = 12) + 
-  theme(axis.text.y = element_text(hjust = 0))
-
-
-
 
 
 
@@ -227,12 +218,12 @@ p_post_sentence <- sentence_forest_data %>%
 
 # Get draws for each study
 speaker_variety_draws <- 
-  spread_draws(native_response_00, r_speaker_variety[Speaker_variety,], b_Intercept) %>% 
+  spread_draws(native_response_01, r_speaker_variety[Speaker_variety,], b_Intercept) %>% 
   mutate(b_Intercept = r_speaker_variety + b_Intercept)
 
 # Get draws for pooled effect
 speaker_variety_pooled_effect_draws <- 
-  spread_draws(native_response_00, b_Intercept) %>% 
+  spread_draws(native_response_01, b_Intercept) %>% 
   mutate(Speaker_variety = "Pooled Effect")
 
 # Combine it and clean up
@@ -264,14 +255,42 @@ p_post_speaker_variety <- speaker_variety_forest_data %>%
     mutate_at(c("b_Intercept", ".lower", ".upper"), strip_blank),
             aes(label = glue("{b_Intercept} [{.lower}, {.upper}]"), x = Inf), 
             hjust = "inward", family = "Times") + 
-  geom_tile(data = speaker_variety_pooled_summary, aes(width = .lower - .upper),
-    alpha = 0.2, height = Inf, fill = "#31688EFF") +
-  stat_pointinterval(point_fill = "white", shape = 21, .width = c(0.8, 0.95)) +
-  coord_cartesian(xlim = c(0, 6)) + 
+  geom_rect(data = speaker_variety_pooled_summary, 
+    aes(xmin = .lower, xmax = .upper, ymin = -Inf, ymax = Inf),
+    alpha = 0.2, fill = "#31688EFF") +
+  stat_halfeye(point_fill = "white", shape = 21, .width = c(0.8, 0.95)) +
+  coord_cartesian(xlim = c(0, 8)) + 
   labs(x = expression(italic("beta")), y = NULL) +
   minimal_adj() + 
   theme(axis.text.y = element_text(hjust = 0))
 
 p_post_participant
-p_post_sentence
 p_post_speaker_variety
+
+
+
+
+
+as_tibble(native_response_01) %>% glimpse()
+
+ranef(native_response_01)$`speaker_variety:sentence_type:sentence`
+ranef(native_response_01)$`speaker_variety:sentence_type`
+ranef(native_response_01)$speaker_variety
+ranef(native_response_01)$sentence_type 
+ranef(native_response_01)$participant 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
