9427933551bd4fa4c39613b31b1e313d1146475c
jvcasillas <joseph.casillas@rutgers.edu>
2021-12-21 02:08:00 -0500

    finish dprime, add ddm and dprime tables, include all tables and figure in supplemantary materials

--- scripts/r/10_dprime.R
+++ scripts/r/10_dprime.R
@@ -1,6 +1,9 @@
 # Dprime Models ---------------------------------------------------------------
 #
-# This script sources libraries and helpers and loads all tidy data
+# Author: Joseph V. Casillas
+# Last update: 20211220
+#
+# - This script contains exploratory d' analyses, plots, and tables
 #
 # -----------------------------------------------------------------------------
 
@@ -16,7 +19,7 @@ source(here::here("scripts", "r", "07_load_data.R"))
 
 # Calculate dprime ------------------------------------------------------------
 
-learners_dp <- learners %>% 
+learners_hr_fa <- learners %>% 
   mutate(
     response_type = case_when(
       sentence_type == "interrogative-partial-wh" & correct_response == 1 & response == 1 ~ "hit",
@@ -45,7 +48,9 @@ learners_dp <- learners %>%
     is_fa = if_else(response_type == "fa", 1, 0),
     is_miss = if_else(response_type == "miss", 1, 0),
     is_cr = if_else(response_type == "cr", 1, 0)
-  ) %>% 
+  ) 
+
+learners_dp_sv <- learners_hr_fa %>% 
   group_by(participant, speaker_variety, lextale_std, eq_std) %>% 
   summarize(
     n_hit = sum(is_hit),
@@ -61,6 +66,21 @@ learners_dp <- learners %>%
     group_by(learners, participant, spn_variety) %>% 
     summarize(.groups = "drop"), by = "participant")
 
+learners_dp_st <- learners_hr_fa %>% 
+  group_by(participant, sentence_type, lextale_std, eq_std) %>% 
+  summarize(
+    n_hit = sum(is_hit) + 1,
+    n_fa = sum(is_fa) + 1,
+    n_miss = sum(is_miss) + 1,
+    n_cr = sum(is_cr) + 1, .groups = "drop") %>% 
+  mutate(n_targets = n_hit + n_miss,
+         n_distractors = n_fa + n_cr) %>% 
+  mutate(dprime = dprime(n_hit, n_fa, n_miss, n_cr, adjusted = T), 
+    sentence_type = fct_relevel(sentence_type, "interrogative-total-yn")) %>% 
+  left_join(., 
+    group_by(learners, participant, spn_variety) %>% 
+    summarize(.groups = "drop"), by = "participant")
+
 # -----------------------------------------------------------------------------
 
 
@@ -68,13 +88,11 @@ learners_dp <- learners %>%
 
 # Models ----------------------------------------------------------------------
 
-
-
 learner_dp_01 <- brm(
   formula = dprime ~ 0 + Intercept + speaker_variety + lextale_std + 
     (1 + speaker_variety | participant), 
   family = "gaussian", 
-  data = learners_dp, 
+  data = learners_dp_sv, 
   prior = c(
     prior(normal(2, 1.5), class = b, coef = "Intercept"),
     prior(normal(0, 1.5), class = b),
@@ -85,6 +103,22 @@ learner_dp_01 <- brm(
   file = here("models", "learner_dp_01")
 )
 
+learner_dp_02 <- brm(
+  formula = dprime ~ 0 + Intercept + sentence_type + lextale_std + 
+    (1 + sentence_type | participant), 
+  family = "gaussian", 
+  data = learners_dp_st, 
+  prior = c(
+    prior(normal(2, 1.5), class = b, coef = "Intercept"),
+    prior(normal(0, 1.5), class = b),
+    prior(cauchy(0, 1), class = sd), 
+    prior(lkj(2), class = cor)), 
+  warmup = 2000, iter = 4000, chains = 4, cores = 4, 
+  control = list(adapt_delta = 0.9999, max_treedepth = 15), 
+  backend = "cmdstanr", 
+  file = here("models", "learner_dp_02")
+)
+
 # -----------------------------------------------------------------------------
 
 
@@ -93,34 +127,99 @@ learner_dp_01 <- brm(
 
 # Exploratory plots -----------------------------------------------------------
 
-conditional_effects(learner_dp_01)
+plot_dp_variety <- as_tibble(learner_dp_01) %>% 
+  select("b_Intercept", starts_with("b_speaker_variety"), -contains(":")) %>% 
+  transmute(
+    Peninsular = b_Intercept, 
+    Andalusian = b_Intercept + b_speaker_varietyandalusian, 
+    Argentine = b_Intercept + b_speaker_varietyargentine, 
+    Chilean = b_Intercept + b_speaker_varietychilean, 
+    Cuban = b_Intercept + b_speaker_varietycuban, 
+    Mexican = b_Intercept + b_speaker_varietymexican, 
+    Peruvian = b_Intercept + b_speaker_varietyperuvian, 
+    `Puerto Rican` = b_Intercept + b_speaker_varietypuertorican
+  ) %>% 
+  pivot_longer(everything(), names_to = "variety", values_to = "estimate") %>% 
+  mutate(variety = fct_reorder(variety, estimate, max)) %>% 
+  ggplot(., aes(x = estimate, y = variety)) + 
+    stat_halfeye(slab_fill = "#cc0033", pch = 21, point_fill = "white") + 
+    scale_y_discrete(position = "right") + 
+    labs(y = "Variety", x = "d'") + 
+    ds4ling::ds4ling_bw_theme(base_family = "Times", base_size = 13) + 
+    theme(axis.title.y = element_text(hjust = 0.05))
 
-learners_dp %>% 
-  ggplot(., aes(x = eq_std, y = dprime, color = speaker_variety)) + 
-    facet_grid(. ~ speaker_variety) + 
-    geom_point() + 
-    geom_smooth(method = lm, formula = "y ~ x")
+plot_dp_utterance <- as_tibble(learner_dp_02) %>% 
+  select("b_Intercept", starts_with("b_sentence_type"), -contains(":")) %>% 
+  transmute(
+    `Interrogative\ny/n` = b_Intercept, 
+    `Interrogative\nwh-` = b_Intercept + b_sentence_typeinterrogativeMpartialMwh, 
+    `Declarative\nnarrow focus` = b_Intercept + b_sentence_typedeclarativeMnarrowMfocus, 
+    `Declarative\nbroad focus` = b_Intercept + b_sentence_typedeclarativeMbroadMfocus
+  ) %>% 
+  pivot_longer(everything(), names_to = "ut", values_to = "estimate") %>% 
+  mutate(ut = fct_reorder(ut, estimate, max)) %>% 
+  ggplot(., aes(x = estimate, y = ut)) + 
+    stat_halfeye(slab_fill = "#cc0033", pch = 21, point_fill = "white") + 
+    labs(y = "Utterance type", x = "d'") + 
+    ds4ling::ds4ling_bw_theme(base_family = "Times", base_size = 13)
 
-learners %>% 
-  group_by(participant, spn_variety) %>% 
-  summarize(.groups = "drop") %>% 
-  group_by(spn_variety) %>% 
-  summarize(n = n(), .groups = "drop") %>% 
-  arrange(desc(n)) %>% 
-  slice(1:4) %>% 
-  ggplot(., aes(x = n, y = spn_variety)) + 
-    geom_segment(aes(x = 0, xend = n, yend = spn_variety)) + 
-    geom_point()
+learner_dp_utterance_variety <- plot_dp_utterance + plot_dp_variety
 
-learners_dp %>% 
-  mutate(familiarity = case_when(
-    speaker_variety == "castilian" & spn_variety == "Spain" ~ "familiar", 
-    speaker_variety == "mexican" & spn_variety == "Mexico" ~ "familiar", 
-    speaker_variety == "castilian" & spn_variety != "Spain" ~ "unfamiliar", 
-    speaker_variety == "mexican" & spn_variety != "Mexico" ~ "unfamiliar", 
-    TRUE ~ "nnd"
-  )) %>% 
-  filter(familiarity != "nnd") %>% 
-  ggplot(., aes(x = familiarity, y = dprime, color = familiarity)) + 
-    geom_beeswarm(alpha = 0.3) + 
-    stat_summary(fun.data = mean_sdl, geom = "pointrange", fun.args = list(mult = 1))
+# -----------------------------------------------------------------------------
+
+
+
+
+# Save plots ------------------------------------------------------------------
+
+devices     <- c('png', 'pdf')
+path_to_fig <- file.path(here("figs", "manuscript"))
+
+walk(devices, ~ ggsave(
+  filename = glue(path_to_fig, "/learner_dp_utterance_variety.", .x), 
+  plot = learner_dp_utterance_variety, 
+  device = .x, height = 4, width = 8.5, units = "in"))
+
+# -----------------------------------------------------------------------------
+
+
+
+
+# Tables ----------------------------------------------------------------------
+
+bind_rows(
+  as_tibble(learner_dp_02) %>% 
+    select("b_Intercept", starts_with("b_sentence_type"), -contains(":")) %>% 
+    transmute(
+      `Interrogative y/n` = b_Intercept, 
+      `Interrogative wh-` = b_Intercept + b_sentence_typeinterrogativeMpartialMwh, 
+      `Declarative narrow focus` = b_Intercept + b_sentence_typedeclarativeMnarrowMfocus, 
+      `Declarative broad focus` = b_Intercept + b_sentence_typedeclarativeMbroadMfocus
+    ) %>% 
+    pivot_longer(everything(), names_to = "Parameter", values_to = "estimate") %>% 
+    mutate(Model = "Utterance type"), 
+
+  as_tibble(learner_dp_01) %>% 
+    select("b_Intercept", starts_with("b_speaker_variety"), -contains(":")) %>% 
+    transmute(
+      Peninsular = b_Intercept, 
+      Andalusian = b_Intercept + b_speaker_varietyandalusian, 
+      Argentine = b_Intercept + b_speaker_varietyargentine, 
+      Chilean = b_Intercept + b_speaker_varietychilean, 
+      Cuban = b_Intercept + b_speaker_varietycuban, 
+      Mexican = b_Intercept + b_speaker_varietymexican, 
+      Peruvian = b_Intercept + b_speaker_varietyperuvian, 
+      `Puerto Rican` = b_Intercept + b_speaker_varietypuertorican
+    ) %>% 
+    pivot_longer(everything(), names_to = "Parameter", values_to = "estimate") %>% 
+    mutate(Model = "Variety")) %>% 
+  group_by(Model, Parameter) %>% 
+  summarize(Median = median(estimate), lower = pull(hdi(estimate)[2]), 
+    upper = pull(hdi(estimate)[3]), MPE = pd(estimate)[1], .groups = "drop") %>% 
+  mutate_if(is.numeric, specify_decimal, k = 2) %>% 
+  mutate(across(-c("Model", "Parameter"), printy::fmt_minus_sign)) %>% 
+  mutate(HDI = glue("[{lower}, {upper}]")) %>% 
+  select(Model, Parameter, Median, HDI, MPE) %>% 
+  write_csv(here("tables", "dp_utterance_variety.csv"))
+
+# -----------------------------------------------------------------------------
--- scripts/r/11_tables.R
+++ scripts/r/11_tables.R
@@ -1,9 +1,11 @@
 # Tables ----------------------------------------------------------------------
 #
+# Author: Joseph V. Casillas
+# Last update: 20211220
 #
-#
-#
-#
+# - This script loads all models and generates summary tables
+# - Tables are save as CSV files (tables/) and can be referenced
+#   by row using report_posterior
 #
 # -----------------------------------------------------------------------------
 
@@ -20,6 +22,7 @@ learner_response_qonly_01 <- readRDS(here("models", "learner_response_qonly_01.r
 learner_response_yn_01 <- readRDS(here("models", "learner_response_yn_01.rds"))
 ddm_boundary_separation <- readRDS(here("models", "ddm_boundary_separation.rds"))
 ddm_drift_rate <- readRDS(here("models", "ddm_drift_rate.rds"))
+learner_dp_01 <- readRDS(here("models", "learner_dp_01.rds"))
 
 # -----------------------------------------------------------------------------
 
@@ -41,18 +44,53 @@ describe_posterior(learner_response_01, rope_range = c(-0.1, 0.1)) %>%
     Parameter == "b_sentence_typeinterrogativeMpartialMwh:lextale_std" ~ "Int. wh-:LexTALE", 
     Parameter == "b_sentence_typedeclarativeMnarrowMfocus:lextale_std" ~ "Dec. narrow focus:LexTALE", 
     Parameter == "b_sentence_typedeclarativeMbroadMfocus:lextale_std" ~ "Dec. broad focus:LexTALE", 
-    Parameter == "b_sentence_typeinterrogativeMpartialMwh:eq_std" ~ "Int. wh-:Empathy quotient", 
-    Parameter == "b_sentence_typedeclarativeMnarrowMfocus:eq_std" ~ "Dec. narrow focus:Empathy quotient", 
-    Parameter == "b_sentence_typedeclarativeMbroadMfocus:eq_std" ~ "Dec. broad focus:Empathy quotient", 
-    Parameter == "b_lextale_std:eq_std" ~ "LexTALE:Empathy quotient", 
-    Parameter == "b_sentence_typeinterrogativeMpartialMwh:lextale_std:eq_std" ~ "Int. wh-:LexTALE:Empathy quotient", 
-    Parameter == "b_sentence_typedeclarativeMnarrowMfocus:lextale_std:eq_std" ~ "Dec. narrow focus:LexTALE:Empathy quotient", 
-    Parameter == "b_sentence_typedeclarativeMbroadMfocus:lextale_std:eq_std" ~ "Dec. broad focus:LexTALE:Empathy quotient"
+    Parameter == "b_sentence_typeinterrogativeMpartialMwh:eq_std" ~ "Int. wh-:EQ", 
+    Parameter == "b_sentence_typedeclarativeMnarrowMfocus:eq_std" ~ "Dec. narrow focus:EQ", 
+    Parameter == "b_sentence_typedeclarativeMbroadMfocus:eq_std" ~ "Dec. broad focus:EQ", 
+    Parameter == "b_lextale_std:eq_std" ~ "LexTALE:EQ", 
+    Parameter == "b_sentence_typeinterrogativeMpartialMwh:lextale_std:eq_std" ~ "Int. wh-:LexTALE:EQ", 
+    Parameter == "b_sentence_typedeclarativeMnarrowMfocus:lextale_std:eq_std" ~ "Dec. narrow focus:LexTALE:EQ", 
+    Parameter == "b_sentence_typedeclarativeMbroadMfocus:lextale_std:eq_std" ~ "Dec. broad focus:LexTALE:EQ"
   )) %>% 
-  mutate_if(is.numeric, specify_decimal, k = 2) %>% 
+  mutate(across(-c("Parameter", "ESS"), specify_decimal, k = 2)) %>% 
+  mutate(ESS = round(ESS)) %>% 
   mutate(across(-Parameter, printy::fmt_minus_sign)) %>% 
   mutate(HDI = glue("[{CI_low}, {CI_high}]")) %>% 
   select(Parameter, Median, HDI, `% in ROPE` = ROPE_Percentage, MPE = pd, ESS) %>% 
   write_csv(here("tables", "learner_response_01.csv"))
+
+# -----------------------------------------------------------------------------
+
+
+
+
+# Boundary separation and drift rate ------------------------------------------
+
+bind_rows(
+  describe_posterior(ddm_boundary_separation) %>% 
+    as_tibble() %>% 
+    select(-c("CI", "ROPE_CI", "ROPE_low", "ROPE_high", "Rhat")) %>% 
+    mutate(Model = "Boundary separation"), 
+  describe_posterior(ddm_drift_rate) %>% 
+    as_tibble() %>% 
+    select(-c("CI", "ROPE_CI", "ROPE_low", "ROPE_high", "Rhat")) %>% 
+    mutate(Model = "Drift rate")
+  ) %>% 
+  mutate(Parameter = case_when(
+    Parameter == "b_Intercept" ~ "Intercept", 
+    Parameter == "b_q_sum" ~ "Question type", 
+    Parameter == "b_lextale_std" ~ "LexTALE", 
+    Parameter == "b_eq_std" ~ "EQ", 
+    Parameter == "b_q_sum:lextale_std" ~ "Question type:LexTALE", 
+    Parameter == "b_q_sum:eq_std" ~ "Question type:EQ", 
+    Parameter == "b_lextale_std:eq_std" ~ "LexTALE:EQ", 
+    TRUE ~ "Question type:LexTALE:EQ"
+  )) %>% 
+  mutate(across(-c("Parameter", "ESS", "Model"), specify_decimal, k = 2)) %>% 
+  mutate(ESS = round(ESS)) %>% 
+  mutate(across(-c("Parameter", "ESS", "Model"), printy::fmt_minus_sign)) %>% 
+  mutate(HDI = glue("[{CI_low}, {CI_high}]")) %>% 
+  select(Model, Parameter, Median, HDI, `% in ROPE` = ROPE_Percentage, MPE = pd, ESS) %>% 
+  write_csv(here("tables", "ddm_bs_dr.csv"))
 
 # -----------------------------------------------------------------------------
