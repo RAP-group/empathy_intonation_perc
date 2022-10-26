3e7d0ded168c353bf10df18ed52a71715a257879
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2022-10-25 21:24:55 -0400

    stimuli analysis updates, add figs, tables

--- scripts/r/14_stimuli_acoustics.R
+++ scripts/r/14_stimuli_acoustics.R
@@ -17,7 +17,7 @@
 
 # Source libs and helpers -----------------------------------------------------
 
-source(here::here("scripts", "r","01_helpers.R"))
+source(here::here("scripts", "r", "07_load_data.R"))
 
 # -----------------------------------------------------------------------------
 
@@ -77,66 +77,174 @@ stim_data <- stim_data_raw %>%
 
 # Data viz --------------------------------------------------------------------
 
-stim_data <- read_csv(here("data", "tidy", "stimuli_acoustics_tidy.csv"))
+stim_data <- read_csv(here("data", "tidy", "stimuli_acoustics_tidy.csv")) %>% 
+  mutate(variety_lab = case_when(
+    variety == "andalusian" ~ "Andalusian", 
+    variety == "argentine" ~ "Argentine", 
+    variety == "castilian" ~ "Madrileño", 
+    variety == "chilean" ~ "Chilean", 
+    variety == "cuban" ~ "Cuban", 
+    variety == "mexican" ~ "Mexican", 
+    variety == "peruvian" ~ "Peruvian", 
+    variety == "puertorican" ~ "Puerto Rican"), 
+    type_lab = case_when(
+      type == "interrogative-total-yn" ~ "Interrogative y/n", 
+      type == "interrogative-partial-wh" ~ "Interrogative Wh-", 
+      type == "declarative-narrow-focus" ~ "Declarative narrow focus", 
+      type == "declarative-broad-focus" ~ "Declarative broad focus"
+    ))
 
 # F0 as a function of sentence type and variety
-stim_data %>% 
+stimuli_pitch_contours <- stim_data %>% 
   filter(type != "filler", f0_log_z <= 2.5, f0_log_z >= -2.5) %>% 
   ggplot() + 
-  aes(x = time_bin, y = f0_log_z, color = variety, fill = variety) + 
-  facet_grid(variety ~ type) + 
-  geom_line(aes(group = item), stat = "smooth", alpha = 0.1, se = F, 
+  aes(x = time_bin, y = f0_log_z, color = variety_lab, fill = variety_lab) + 
+  facet_grid(variety_lab ~ type_lab) + 
+  geom_line(aes(group = item), stat = "smooth", alpha = 0.15, se = F, 
     show.legend = F, formula = "y ~ x", method = "loess") + 
-  geom_smooth(method = "loess", span = 0.5, formula = "y ~ x", size = 1.2, 
+  geom_smooth(method = "loess", span = 0.5, formula = "y ~ x", size = 1.1, 
     show.legend = F) + 
   scale_color_manual(values = viridis::viridis_pal(
     option = "B", begin = 0.3, end = 0.8)(8)) + 
   scale_fill_manual(values = viridis::viridis_pal(
     option = "B", begin = 0.3, end = 0.8)(8)) + 
-  scale_x_continuous(labels = scales::percent_format(scale = 1)) + 
+  scale_x_continuous(labels = scales::label_percent(scale = 1)) + 
+  coord_cartesian(xlim = c(0, 100)) + 
   labs(y = NULL, x = NULL) + 
-  ds4ling::ds4ling_bw_theme(base_size = 12, base_family = "Times") 
+  ds4ling::ds4ling_bw_theme(base_size = 8, base_family = "Times") + 
+  theme(strip.background = element_rect(fill = NA), 
+        axis.text.x = element_text(size = rel(0.75)), 
+        axis.text.y = element_text(size = rel(0.75)))
 
-# Spectrogram
+walk(c('png', 'pdf'), ~ ggsave(
+  filename = glue(file.path(here("figs", "manuscript")), "/stimuli_pitch_contours.", .x), 
+  plot = stimuli_pitch_contours, 
+  device = .x, height = 6, width = 5, units = "in"))
 
-filename <- "andalusian_match_declarative-broad-focus_Ana-lleva-el-abrigo"
-ext_in   <- ".wav"
-ext_out  <- ".png"
-wav <- here("exp", "empathy_intonation_perc", "stim", "wavsx", paste0(filename, ext_in))
-speakr::praat_plot(
-  #here("figs", "stimuli", glue(filename, ext_out)),
-  "./figs/stimuli/andalusian_match_declarative-broad-focus_Ana-lleva-el-abrigo.png", 
-  wav, 
-  f0 = TRUE, 
-  f0_max = 400 
+# -----------------------------------------------------------------------------
+
+
+
+
+# Accuracy --------------------------------------------------------------------
+
+l2_native_accuracy <- bind_rows(
+  learners %>% 
+    select(participant, speaker_variety, sentence_type, is_correct) %>% 
+    mutate(group = "L2"),
+  
+  natives %>% 
+    select(participant, speaker_variety, sentence_type, is_correct) %>% 
+    mutate(group = "Native")
+  ) %>% 
+  mutate(speaker_variety = case_when(
+      speaker_variety == "andalusian" ~ "Andalusian", 
+      speaker_variety == "argentine" ~ "Argentine", 
+      speaker_variety == "castilian" ~ "Madrileño", 
+      speaker_variety == "chilean" ~ "Chilean", 
+      speaker_variety == "cuban" ~ "Cuban", 
+      speaker_variety == "mexican" ~ "Mexican", 
+      speaker_variety == "peruvian" ~ "Peruvian", 
+      speaker_variety == "puertorican" ~ "Puerto Rican")
+  ) %>% 
+  group_by(group, sentence_type, speaker_variety) %>% 
+  summarize(avg = mean_se(is_correct), .groups = "drop")
+
+learner_native_accuracy <- l2_native_accuracy %>% 
+  transmute(group = str_replace(group, "Native", "Monolingual"), 
+    Type = case_when(
+      sentence_type == "interrogative-total-yn" ~ "Interrogative\ny/n", 
+      sentence_type == "interrogative-partial-wh" ~ "Interrogative\nWh-", 
+      sentence_type == "declarative-narrow-focus" ~ "Declarative\nnarrow focus", 
+      sentence_type == "declarative-broad-focus" ~ "Declarative\nbroad focus"), 
+    Variety = speaker_variety, 
+    avg = avg$y, lower = .$avg$ymin, upper = .$avg$ymax) %>% 
+  ggplot() + 
+  aes(x = Type, y = avg, color = Variety) + 
+  facet_grid(. ~ group) + 
+  geom_hline(yintercept = 0.5, lty = 3) + 
+  geom_errorbar(aes(ymin = lower, ymax = upper), show.legend = F, 
+    position = position_dodge(0.5), width = 0) +
+  geom_point(position = position_dodge(0.5)) + 
+  scale_color_manual(name = NULL, 
+    values = viridis::viridis_pal(option = "C", begin = 0.2, end = 0.8)(8)) + 
+  coord_cartesian(ylim = c(0, 1.02), xlim = c(0.6, 4.4), expand = F) + 
+  scale_y_continuous(labels = scales::percent_format()) + 
+  labs(y = "Accuracy", x = NULL) + 
+  ds4ling::ds4ling_bw_theme(base_size = 12, base_family = "Times") + 
+  theme(legend.position = c(0.1, 0.25), legend.background = element_blank(), 
+    legend.key.size = unit(0.35, "cm"), 
+    strip.background = element_rect(fill = NA))
+
+walk(c('png', 'pdf'), ~ ggsave(
+  filename = glue(file.path(here("figs", "manuscript")), "/learner_native_accuracy.", .x), 
+  plot = learner_native_accuracy, 
+  device = .x, height = 4, width = 8.5, units = "in"))
+
+l2_native_accuracy_tbl <- l2_native_accuracy %>% 
+  pivot_wider(names_from = "group", values_from = "avg") %>% 
+  transmute(
+    Type = sentence_type, Variety = speaker_variety, 
+    L2 = glue("{specify_decimal(L2$y, 2)} [{specify_decimal(L2$ymin, 2)}, {specify_decimal(L2$ymax, 2)}]"),
+    Monolingual = glue("{specify_decimal(Native$y, 2)} [{specify_decimal(Native$ymin, 2)}, {specify_decimal(Native$ymax, 2)}]")
+  ) %>% 
+  mutate(Type = case_when(
+    Type == "interrogative-total-yn" ~ "Interrogative y/n", 
+    Type == "interrogative-partial-wh" ~ "Interrogative Wh-", 
+    Type == "declarative-narrow-focus" ~ "Declarative narrow focus", 
+    Type == "declarative-broad-focus" ~ "Declarative broad focus"
+  )) %>% 
+  write_csv(here("tables", "learner_native_accuracy.csv"))
+
+# -----------------------------------------------------------------------------
+
+
+
+
+# Spectrogram stuff -----------------------------------------------------------
+
+# Path to script
+plot_script <- here("scripts", "praat", "6_plot_spectrogram.praat")
+
+wav1 <- here("data", "stimuli", "sounds", "andalusian_match_interrogative-partial-wh_Por-que-abre-el-regalo.wav")
+tg1  <- here("data", "stimuli", "textgrids", "andalusian_match_interrogative-partial-wh_Por-que-abre-el-regalo.textgrid")
+out1 <- here("figs", "stimuli", "andalusian_match_interrogative-partial-wh_Por-que-abre-el-regalo.pdf") 
+
+# Run script, capture output, read as csv
+speakr::praat_run(
+  plot_script, 
+  file = out1, 
+  caption = "Andalusian\\ -\\ Partial\\ interrogative", 
+  wav = wav1, 
+  tg = tg1, 
+  start = 0, 
+  end = 0, 
+  width = 5, 
+  format = "pdf", 
+  pitch = TRUE, 
+  pitch_min = 0, 
+  pitch_max = 500, 
+  hz_max = 5000
   )
 
-library(phonTools)
-sound <- loadsound()
-spectrogram(sound, fs = 44100, colors = TRUE, 
-            maintitle = "Welcome", maxfreq = 5500)
+wav2 <- here("data", "stimuli", "sounds", "andalusian_match_interrogative-partial-wh_Cuando-bebia-el-vino.wav")
+tg2  <- here("data", "stimuli", "textgrids", "andalusian_match_interrogative-partial-wh_Cuando-bebia-el-vino.textgrid")
+out2 <- here("figs", "stimuli", "andalusian_match_interrogative-partial-wh_Cuando-bebia-el-vino.pdf") 
 
-
-library("tuneR")
-library("seewave")
-library("phonTools")
-
-
-polo_wav <- tuneR::readWave(wav)
-input <- seewave::inputw(wave = polo_wav)
-wave <- input$w
-head(wave)
-
-wav_df <- wave %>%
-  as_tibble() %>%
-  rename(hz = V1) %>%
-  tibble::rowid_to_column("sample") %>%
-  mutate(t = sample / polo_wav@samp.rate)
-
-ggplot(wav_df, aes(t, hz)) + 
-  geom_line()
-
-phonTools::spectrogram(wav_df$hz)
-
+speakr::praat_run(
+  script = plot_script, 
+  file = out2, 
+  caption = "Andalusian\\ -\\ Partial\\ interrogative", 
+  wav = wav2, 
+  tg = tg2, 
+  start = 0, 
+  end = 0, 
+  width = 5, 
+  format = "pdf", 
+  pitch = TRUE, 
+  pitch_min = 0, 
+  pitch_max = 500, 
+  hz_max = 5000
+)
 
 # -----------------------------------------------------------------------------
