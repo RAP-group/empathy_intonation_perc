eabc44a4ba854431eb2f9e03820792bc24eb1231
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2022-11-03 23:59:32 -0400

    plot spectrograms, generate combined plots, need to adjst textgrids

--- scripts/praat/6_plot_spectrogram.praat
+++ scripts/praat/6_plot_spectrogram.praat
@@ -61,7 +61,7 @@ sp = To Spectrogram: 0.005, hz_max, 0.002, 20, "Gaussian"
 
 if pitch$ != "FALSE"
   selectObject: wav
-  pitch_obj = To Pitch: 0, 75, 600
+  pitch_obj = To Pitch: 0, pitch_min, pitch_max
 endif
 
 # Set up plotting params ############################################
@@ -95,8 +95,8 @@ if pitch$ != "FALSE"
   Draw: start, end, pitch_min, pitch_max, "no"
   Line width: 1
   Black
-  #Axes: start, end, pitch_min, pitch_max
-  #Marks right: 2, "yes", "yes", "no"
+  Axes: start, end, pitch_min, pitch_max
+  Marks left: 2, "yes", "yes", "no"
 endif
 
 ### Spectrogram box #################################################
@@ -131,15 +131,15 @@ int_min = Get minimum: start, end, "sinc70"
 int_max = Get maximum: start, end, "sinc70"
 
 Axes: start, end, int_min, int_max
-One mark right: int_min, "no", "yes", "no", fixed$(int_min, 2)
-One mark right: 0, "yes", "yes", "yes", ""
-One mark right: int_max, "no", "yes", "no", fixed$(int_max, 2)
+#One mark right: int_min, "no", "yes", "no", fixed$(int_min, 2)
+#One mark right: 0, "yes", "yes", "yes", ""
+#One mark right: int_max, "no", "yes", "no", fixed$(int_max, 2)
 
 Select inner viewport: 0, win_width, wav_height, wav_height + sp_height
 
 Axes: start, end, 0, hz_max
-Marks left: 2, "yes", "yes", "no"
-Text left: "no", "Hz"
+#Marks left: 2, "yes", "yes", "no"
+Text left: "no", "F0 (Hz)"
 
 Select inner viewport: 0, win_width, 0, inner_height
 
@@ -149,7 +149,7 @@ Text bottom: "no", caption$
 
 ### Select plot and save ###########################################
 
-Select inner viewport: 0, win_width, 0, inner_height
+Select inner viewport: 0.25, win_width - 0.25, 0.25, inner_height - 0.1
 
 if format$ == "png"
   Save as 600-dpi PNG file: file$
--- scripts/r/14_stimuli_acoustics.R
+++ scripts/r/14_stimuli_acoustics.R
@@ -131,27 +131,54 @@ walk(c('png', 'pdf'), ~ ggsave(
 # Path to script
 plot_script <- here("scripts", "praat", "6_plot_spectrogram.praat")
 
-wavs <- c(
-  here("data", "stimuli", "sounds", "andalusian_match_declarative-broad-focus_Ana-lleva-el-abrigo.wav"), 
-  here("data", "stimuli", "sounds", "andalusian_match_declarative-narrow-focus_Ana-lleva-el-abrigo.wav")
+# Vector of variety names
+variety_names <- c(
+  "andalusian", "argentine", "castilian", "chilean", "cuban", "mexican", 
+  "peruvian", "puertorican"
+)
+
+# Vector of utterances
+utterances <- c(
+  "_match_declarative-broad-focus_Ana-lleva-el-abrigo",
+  "_match_declarative-narrow-focus_Ana-lleva-el-abrigo",
+  "_match_interrogative-partial-wh_Cuando-lleva-el-abrigo",
+  "_match_interrogative-total-yn_Ana-lleva-el-abrigo",
+  "_match_interrogative-total-yn_Mariano-habla-del-tiempo"
   )
 
-tgds <- c(
-  here("data", "stimuli", "textgrids", "andalusian_match_declarative-broad-focus_Ana-lleva-el-abrigo.textgrid"), 
-  here("data", "stimuli", "textgrids", "andalusian_match_declarative-narrow-focus_Ana-lleva-el-abrigo.textgrid")
-  )
+# Generate vector of items
+make_vec_of_items <- function(variety) {
+  out <- as.character(glue("{variety}{utterances}"))
+  return(out)
+}
 
-outs <- c(
-  here("figs", "stimuli", "andalusian_match_declarative-broad-focus_Ana-lleva-el-abrigo.png"), 
-  here("figs", "stimuli", "andalusian_match_declarative-narrow-focus_Ana-lleva-el-abrigo.png")
-  )
+example_items <- map(variety_names, make_vec_of_items) %>% unlist
+wavs <- glue('{here("data", "stimuli", "sounds")}/{example_items}.wav')
+tgds <- glue('{here("data", "stimuli", "textgrids")}/{example_items}.TextGrid')
+outs <- glue('{here("figs", "stimuli")}/{example_items}.png')
 
-caps <- c(
-  "Andalusian\\ -\\ Broad\\ focus", 
-  "Andalusian\\ -\\ Narrow\\ focus"
-  )
+cap_hold <- c(
+  "\\ -\\ Broad\\ focus", 
+  "\\ -\\ Narrow\\ focus", 
+  "\\ -\\ Wh-\\ question", 
+  "\\ -\\ yes/no\\ question", 
+  "\\ -\\ yes/no\\ question"
+)
 
-for (i in 1:length(an_wavs)) {
+variety_caps <- c("Andalusian", "Argentine", "Madrileño", "Chilean", 
+                  "Cuban", "Mexican", "Puerto\\ Rican")
+
+make_vec_of_caps <- function(variety) {
+  out <- as.character(glue("{variety}{cap_hold}"))
+  return(out)
+}
+
+caps <- map(variety_caps, make_vec_of_caps) %>% unlist
+
+f0_mins <- c(rep(75, 5), rep(50, 5), rep(75, 20), rep(50, 5), rep(75, 5))
+f0_maxs <- c(rep(500, 5), rep(350, 5), rep(500, 20), rep(350, 5), rep(500, 5))
+
+for (i in 1:length(wavs)) {
   speakr::praat_run(
     plot_script, 
     file = outs[i], 
@@ -163,23 +190,31 @@ for (i in 1:length(an_wavs)) {
     width = 7, 
     format = "png", 
     pitch = TRUE, 
-    pitch_min = 0, 
-    pitch_max = 500, 
-    hz_max = 5000
+    pitch_min = f0_mins[i], 
+    pitch_max = f0_maxs[i], 
+    hz_max = 5500
   )
 }
 
-library("cowplot")
-path1 <- here("figs", "stimuli", "andalusian_match_interrogative-partial-wh_Cuando-bebia-el-vino.png")
-path2 <- here("figs", "stimuli", "andalusian_match_interrogative-partial-wh_Por-que-abre-el-regalo.png")
-plot1 <- ggdraw() + draw_image(path1)
-plot2 <- ggdraw() + draw_image(path2)
-spectrogram_analuz <- plot_grid(plot1, plot2, labels = "auto", label_y = 0.8)
+# Items to plot
+img_ref <- glue("{utterances[1:4]}.png")
 
-walk(c('png', 'pdf'), ~ ggsave(
-  filename = glue(file.path(here("figs", "stimuli")), "/spectrogram_andaluz.", .x), 
-  plot = spectrogram_analuz, 
-  device = .x, height = 4, width = 8.5, units = "in"))
+# Function to generate plots
+combine_spectrograms <- function(variety) {
+  # Pull .png files of utterance types by variety in 2x2 grid
+  out <- reduce(map(glue('{here("figs", "stimuli")}/{variety}{img_ref}'), 
+        ~ cowplot::ggdraw() + cowplot::draw_image(.x)), `+`
+  )
+  
+  # Save as png and pdf
+  walk(c('png', 'pdf'), ~ ggsave(
+    filename = glue(here("figs", "stimuli"), glue("/spectrogram_{variety}."), .x), 
+    plot = out, 
+    device = .x, height = 6, width = 8.5, units = "in"))
+}
+
+# Generate plot for each variety
+walk(variety_names, ~ combine_spectrograms(variety = .x))
 
 # -----------------------------------------------------------------------------
 
@@ -304,7 +339,7 @@ native_variety_matched_accuracy <- variety_matches %>%
         strip.background = element_rect(fill = NA))
 
 walk(c('png', 'pdf'), ~ ggsave(
-  filename = glue(file.path(here("figs", "manuscript")), "/native_variety_matched_accuracy.", .x), 
+  filename = glue(here("figs", "manuscript"), "/native_variety_matched_accuracy.", .x), 
   plot = native_variety_matched_accuracy, 
   device = .x, height = 4, width = 8.5, units = "in"))
 
